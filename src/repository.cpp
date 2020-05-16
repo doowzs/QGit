c4864a657086d9b1ad46f9ff26e49673660278aa
Tianyun Zhang <doowzs@qq.com>
2020-05-16 18:48:49 +0800

    open file then read it, use try catch to handle errors

--- src/repository.cpp
+++ src/repository.cpp
@@ -17,12 +17,18 @@ Repository::Repository(bool debug, const QString &path, QWidget *parent)
     : QWidget(parent), debug(debug), root(path + "/.git") {
   QDir headsDir = QDir(root + "/refs/heads");
   QStringList headsList = headsDir.entryList(QDir::Files | QDir::NoDotAndDotDot);
-  foreach (QString head, headsList) {
+  for (const QString &head : headsList) {
     QFile headFile = QFile(root + "/refs/heads/" + head);
-    QString hash = QString(headFile.readAll());
-    heads[head] = hash;
-    if (debug) {
-      qDebug() << "ref" << head << hash;
+    try {
+      headFile.open(QFile::ReadOnly);
+      QString hash = QString(headFile.readAll());
+      headFile.close();
+      heads[head] = hash;
+      if (debug) {
+        qDebug() << "ref" << head << hash;
+      }
+    } catch (QException e) {
+      qDebug() << "error:" << e.what() << "when reading ref" << head;
     }
   }
 }
